e3a344e5dfe98bfd47b5e1e1a7be0706571fbd74
dreamer6680 <1468683855@qq.com>
2026-09-19 14:09:59 +0800

    feat: enable LocatorJS source locations in Turbopack

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  turbopack: {
+    rules: {
+      "**/*.{tsx,jsx}": {
+        loaders: [{ loader: "@locator/webpack-loader", options: { env: "development" } }],
+      },
+    },
+  },
 };
 
 export default nextConfig;
